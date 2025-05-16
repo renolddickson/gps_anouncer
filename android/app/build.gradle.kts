9d4f177935845571ef60d8b389451df17564b824
renolddickson <renolddickson18@gmail.com>
2025-05-17 00:15:45 +0530

    change

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,7 +12,6 @@ android {
     namespace = "com.example.gps_anouncer"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
-    ndkVersion = "27.0.12077973"
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
